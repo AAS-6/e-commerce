aeb08581c71a179e6ad6fcb3573dd8deafefd1cd
Josiah Farrel <87465571+josiahfs@users.noreply.github.com>
2023-06-07 03:22:06 +0700

    Dev arel profile (#4)
    * create user data input page ui
    
    * add profile page ui
    
    * add wishlist page ui
    
    * create cartlist component

--- src/app/dummy/page.tsx
+++ src/app/dummy/page.tsx
@@ -1,6 +1,13 @@
-"use client"
+"use client";
 import Pulsa from "@/components/digital-product/Pulsa";
+import CartList from "@/components/cart/CartList";
+import BreadCrumb from "@/components/cart/BreadCrumb";
 
 export default function Wrapper() {
-  return <Pulsa />;
+  return (
+    <main>
+      <BreadCrumb />
+      <CartList />
+    </main>
+  );
 }
